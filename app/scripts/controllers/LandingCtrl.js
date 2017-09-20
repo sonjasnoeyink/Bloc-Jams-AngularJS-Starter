2e00c65db3c451510bd12ed6e3bb806c2c2b7f32
sonjasnoeyink <sonja.snoeyink@gmail.com>
2017-09-20 13:11:40 -0400

    Assignment-5: Complete

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -1,8 +1,9 @@
 (function() {
      function LandingCtrl() {
+       this.heroTitle = "Turn the Music Up!";
      }
 
      angular
          .module('blocJams')
          .controller('LandingCtrl', LandingCtrl);
- })();
+})();
